d8bbc4827a717883de4a1239717402c928bb6b15
Eleanor Davies <ellie@mavieson.co.uk>
2024-08-15 15:55:23 +0100

    fix: scope [skip ci]

--- pkg/router/login.go
+++ pkg/router/login.go
@@ -9,16 +9,19 @@ import (
 func (r *Router) Login(username, password string) error {
 	if output, _, err := r.Expect("login: "); err != nil {
 		return fmt.Errorf("failed to get username prompt from router %w", err)
+	} else {
+		logrus.WithField("output", output).Debugln("login field obtained from router")
 	}
-	logrus.WithField("output", output).Debugln("login field obtained from router")
 
 	if _, err := r.Write(fmt.Sprintf("%s\n", username)); err != nil {
 		return fmt.Errorf("failed to write the username to the router: %w", err)
 	}
+	
 	if output, _, err := r.Expect("Password: "); err != nil {
 		return fmt.Errorf("failed to get password prompt from router: %w", err)
+	} else {
+		logrus.WithField("output", output).Debugln("password field obtained from router")
 	}
-	logrus.WithField("output", output).Debugln("password field obtained from router")
 
 	if _, err := r.Write(fmt.Sprintf("%s\n", password)); err != nil {
 		return fmt.Errorf("failed to write the password to the router: %w", err)
