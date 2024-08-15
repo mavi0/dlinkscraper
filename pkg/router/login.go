cb7841f05bfd5884f39ed1c24e1a6e0d1681e796
Eleanor Davies <ellie@mavieson.co.uk>
2024-08-15 15:47:49 +0100

    chore: login debug [skip ci]

--- pkg/router/login.go
+++ pkg/router/login.go
@@ -2,18 +2,25 @@ package router
 
 import (
 	"fmt"
+
+	"github.com/sirupsen/logrus"
+	)
 )
 
 func (r *Router) Login(username, password string) error {
-	if _, _, err := r.Expect("login: "); err != nil {
+	if output, _, err := r.Expect("login: "); err != nil {
 		return fmt.Errorf("failed to get username prompt from router %w", err)
 	}
+	logrus.WithField("output", output).Debugln("login field obtained from router")
+
 	if _, err := r.Write(fmt.Sprintf("%s\n", username)); err != nil {
 		return fmt.Errorf("failed to write the username to the router: %w", err)
 	}
-	if _, _, err := r.Expect("Password: "); err != nil {
+	if output, _, err := r.Expect("Password: "); err != nil {
 		return fmt.Errorf("failed to get password prompt from router: %w", err)
 	}
+	logrus.WithField("output", output).Debugln("password field obtained from router")
+
 	if _, err := r.Write(fmt.Sprintf("%s\n", password)); err != nil {
 		return fmt.Errorf("failed to write the password to the router: %w", err)
 	}
