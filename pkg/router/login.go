4d664c4eb6a347e693e880e566d1512fb6313e05
Eleanor Davies <ellie@mavieson.co.uk>
2024-08-15 16:06:05 +0100

    doc: comments & rebuild

--- pkg/router/login.go
+++ pkg/router/login.go
@@ -6,7 +6,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Login sends the username and password to the router for authentication.
+// It expects to receive prompts for the username and password from the router,
+// and writes the provided username and password to the router.
+// If any error occurs during the process, it returns an error.
 func (r *Router) Login(username, password string) error {
+	
 	if output, _, err := r.Expect("login: "); err != nil {
 		return fmt.Errorf("failed to get username prompt from router %w", err)
 	} else {
@@ -16,7 +21,7 @@ func (r *Router) Login(username, password string) error {
 	if _, err := r.Write(fmt.Sprintf("%s\n", username)); err != nil {
 		return fmt.Errorf("failed to write the username to the router: %w", err)
 	}
-	
+
 	if output, _, err := r.Expect("Password: "); err != nil {
 		return fmt.Errorf("failed to get password prompt from router: %w", err)
 	} else {
